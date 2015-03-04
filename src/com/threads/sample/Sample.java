8a5f8688675e3e4d1ab924433ca5f221b64c7153
venkatasivaram <pvsivarammca@gmail.com>
2015-03-04 17:25:35 +0530

    first  commit in branch new-branch git

--- src/com/threads/sample/Sample.java
+++ src/com/threads/sample/Sample.java
@@ -1,12 +1,14 @@
 package com.threads.sample;
 
+import java.util.Date;
+
 public class Sample {
 	
 	public static void main(String args[])
 	{
 		System.out.println("Git Testing");
 		
-		System.out.println("Git Testing Line Added");
+		System.out.println("Git Branch Testing  "+new Date());
 	}
 
 }
